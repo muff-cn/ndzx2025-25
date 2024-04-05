e923652f09ae0f269cb31a3a1f6ec4fd4d925dd5
木意符韵 <2138543006@qq.com>
2024-04-05 13:14:27 +0800

    optimaze

--- js/clock.js
+++ js/clock.js
@@ -9,6 +9,16 @@ images = [
     'https://img2.imgtp.com/2024/04/05/nuBt3KgE.jpg',
 ]
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+images = shuffleArray(images)
+
 function updateBackground() {
     document.body.style.backgroundImage = `url('${images[currentImageIndex]}')`;
 }
